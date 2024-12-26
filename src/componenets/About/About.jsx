bf33255cc88ef3b41de6ded44512757471675db9
Muhammad Farooqi <m2farooq@uwaterloo.ca>
2024-12-26 11:38:00 -0800

    Update About.jsx

--- src/componenets/About/About.jsx
+++ src/componenets/About/About.jsx
@@ -13,14 +13,14 @@ export const About =()=>{
         <img src={getImageUrl("about/me.webp")} alt="luffy" className={styles.aboutImage}/>
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/uiIcon.png")} alt="Cursor" />
+            <img src={getImageUrl("about/robotics.png")} className={styles.robotImg} alt="Servor" />
             <div className={styles.aboutItemText}>
               <h3>Robotics</h3>
               <p>I am passionate about robotics and humanoids. I have designed projects such as a humanoid arm, a diff-drive robot, and robot simulations using technology like ROS2, Gazebo, Rviz, Arduino, and various sensors and equipment.</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/robotics.png")} className={styles.robotImg} alt="Servor" />
+            <img src={getImageUrl("about/uiIcon.png")} alt="Cursor" />
             <div className={styles.aboutItemText}>
               <h3>Full Stack Development</h3>
               <p>I have experience working with both front-end and back-end applications, creating web applications, landing pages, full-stack apps, and dynamic mobile applications.</p>
